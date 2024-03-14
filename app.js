a37066e82b689a1cd639769473e92265aa3d0d99
Hammad-Hasnain <hammadhasnain81@gmail.com>
2024-03-14 09:20:24 +0500

    CRUD Backend first commit with login-signup

---
+++ app.js
@@ -0,0 +1,98 @@
+import express, { response } from 'express'
+import mongoose from 'mongoose';
+import postModel from './model/postSchema.js';
+import bcrypt, { compare } from 'bcrypt'
+import dotenv from 'dotenv'
+dotenv.config()
+
+
+const app = express()
+const PORT = process.env.PORT || 5000
+
+
+
+// DB connecting
+mongoose.connect(process.env.URI)
+mongoose.connection.on("connected", () => { console.log('Mongo db connected') })
+mongoose.connection.on("error", (err) => { console.log('Mongo db not connected==>', err) })
+
+
+// body parser
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+
+// get API
+app.get('/', async (req, res) => {
+    res.json({
+        message: "SERVER UP"
+    })
+})
+
+
+// SIGNUP API
+app.post('/api/signUp', async (req, res) => {
+    try {
+        const { firstName, lastName, email, password } = req.body
+
+        const hashedPassword = await bcrypt.hash(password, 10)
+
+
+        const userObj = {
+            ...req.body,
+            password: hashedPassword,
+        }
+
+        const isEmail = await postModel.findOne({ email })
+
+        if (!isEmail) {
+            await postModel.create(userObj)
+        }
+        else {
+            res.json({ message: 'Email already exists' })
+            return
+        }
+
+    } catch (error) {
+        console.log(error);
+    }
+
+})
+
+
+// LOGIN API
+app.post('/api/login', async (req, res) => {
+    const { email, password } = req.body
+
+
+    if (!email || !password) {
+        res.json({
+            message: 'required credentials are missing'
+        })
+        return
+    }
+    else {
+        const user = await postModel.findOne({ email })
+        const hashedPassword = user.password
+
+        const compared = await bcrypt.compare(password, hashedPassword)
+        if (compared) {
+            res.json({
+                message: 'successfully login',
+                data: 'email exist'
+            })
+        }
+        else {
+            res.json({
+                message: 'Invalid credentials'
+            })
+        }
+    }
+})
+
+
+
+//SERVER
+app.listen(PORT, () => {
+    console.log('SERVER UP')
+})
